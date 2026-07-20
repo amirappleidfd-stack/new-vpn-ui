fc1539928ac0d6dcd88be3b9163de1166bb6fdf4
amir shayanfar <amirappleidfd@outlook.com>
2026-07-21 00:10:58 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
-echo "🚀 Starting X-UI on port ${PORT}..."
-# تنظیم پورت در فایل کانفیگ
+
+echo "🚀 Starting X-UI on port ${PORT}"
+
 mkdir -p /etc/x-ui
-cat > /etc/x-ui/config.json << EOF
+
+
+cat > /etc/x-ui/config.json <<EOF
 {
-  "webPort": ${PORT},
-  "webBasePath": "/",
-  "webListen": "0.0.0.0",
-  "logLevel": "info"
+  "webPort": ${PORT},
+  "webBasePath": "/",
+  "webListen": "0.0.0.0",
+  "logLevel": "info"
 }
 EOF
-# اجرای X-UI
+
+
 cd /usr/local/x-ui
-./x-ui
+
+exec ./x-ui
